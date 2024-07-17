51850614d85956691812a4ded24a5aea9df98572
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-07-16 23:00:38 -0300

    clase00

---
+++ clase_00/index.js
@@ -0,0 +1,38 @@
+// clase 00
+
+// console.log("Hola");
+// console.log("1");
+// console.log(true);
+// console.log([1, 2, 3, "Hola", true]);
+
+
+// Tipos de datos
+let nombre // <-- declaracion de la variable, y es mutable
+nombre = "Jorge" // <-- asignacion de str
+
+nombre = "Jesus" // <-- asignacion de str
+
+console.log(nombre);
+
+
+// Tipos de datos
+console.log(typeof nombre);
+
+let number = 12
+console.log(typeof number);
+
+
+const obj = { clave: "Valor" }
+console.log(obj);
+console.log(typeof obj);
+
+let array = [1, 2, 3, "Hola"]
+console.log(array);
+console.log(typeof array);
+
+
+// No mutables 
+const nombre2 = "Eli"
+nombre2 = "Juan"
+
+console.log(nombre2);
